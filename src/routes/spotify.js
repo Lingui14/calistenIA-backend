5349611db82a413549e6d284881199da14491c1b
sebastiux <130516918+sebastiux@users.noreply.github.com>
2025-11-22 20:31:14 -0600

    food error

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -6,7 +6,6 @@ const { UserContext } = require('../models');
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
 const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
-
 /**
  * GET /api/spotify/auth-url
  * Genera URL de autorización de Spotify
