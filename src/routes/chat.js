57f4635dccb24914487b52ba8430d8b5c1350c0a
sebastiux <130516918+sebastiux@users.noreply.github.com>
2025-11-24 14:52:47 -0600

    Rutinas perronas 10

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -138,7 +138,7 @@ router.post('/', auth, async (req, res) => {
         ],
         tools: AVAILABLE_FUNCTIONS.map(f => ({ type: 'function', function: f })),
         tool_choice: 'auto',
-        max_tokens: 1000
+        max_tokens: 9000
       })
     });
 
