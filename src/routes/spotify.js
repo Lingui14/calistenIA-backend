6e3503110dc6a038932b48cadbb70fea33bc6f82
Lingui14 <@A223787A@correo.uia.mx>
2025-11-25 16:58:41 -0600

    Hardcodera variable URI

--- src/routes/spotify.js
+++ src/routes/spotify.js
@@ -5,7 +5,7 @@ const { UserContext } = require('../models');
 
 const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
 const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
+const SPOTIFY_REDIRECT_URI = 'calistenia://spotify-callback';  // HARDCODEADO
 
 /**
  * GET /api/spotify/auth-url
